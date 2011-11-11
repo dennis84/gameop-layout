3e0da6aac2d6098ae61aec9230b04d7a497449fc
Dennis Dietrich <d.dietrich84@googlemail.com>
2011-11-11 18:09:49 +0100

    updated layout

--- js/form-fields.js
+++ js/form-fields.js
@@ -1,21 +1,59 @@
-$(function () {
-    $('.form-expanded-choice-row').each(function (event) {
-        var field  = $(this);
-        var fields = $(this).find('input');
-        var target = field.find('> div');
-        var fake   = field.find('.form-field-value');
 
-        fake.bind('click', function () {
+/*
+ * This file is part of the gameop package.
+ *
+ * (c) Dennis Dietrich <d.dietrich84@googlemail.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$(function () {
+    $('.form-expanded-choice-row').each(function () {
+        var row      = $(this);
+        var target   = row.find('> div');
+        var dropdown = row.find('.form-dropdown-field');
+
+        dropdown.bind('click', function () {
             target.toggle();
         });
 
         target.find('input').change(function () {
             target.find('li').removeClass('checked');
             $(this).closest('li').addClass('checked');
-            fake.text($(this).next('label').text());
+            dropdown.text($(this).next('label').text());
             setTimeout(function () {
                 target.hide()
             }, 100);
         })
+
+        target.find('.footer-action a').bind('click', function () {
+            target.hide();
+            return false;
+        });
+    });
+
+    $('.form-expanded-multiple-choice-row').each(function () {
+        var row      = $(this);
+        var target   = row.find('> div');
+        var dropdown = row.find('.form-dropdown-field');
+
+        dropdown.bind('click', function () {
+            target.toggle();
+        });
+
+        target.find('input').change(function () {
+            target.find('li').removeClass('checked');
+            $(this).closest('li').addClass('checked');
+            dropdown.text($(this).next('label').text());
+            setTimeout(function () {
+                target.hide()
+            }, 100);
+        })
+
+        target.find('.footer-action a').bind('click', function () {
+            target.hide();
+            return false;
+        });
     });
 });
