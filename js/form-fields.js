3110367e460361e7c36441b76293750738bd630f
Dennis Dietrich <d.dietrich84@googlemail.com>
2011-11-14 19:14:19 +0100

    fixed slider update func

--- js/form-fields.js
+++ js/form-fields.js
@@ -9,7 +9,7 @@
  */
 
 $(function () {
-    $('#price').append('<div id="price-slider-range"></div>');
+    //$('#price').append('<div id="price-slider-range"></div>');
 
     $('#price-slider-range').slider({
         range: true,
@@ -17,11 +17,11 @@ $(function () {
         max: 100,
         values: [ 0, 20 ],
         slide: function(event, ui) {
-            $('#amount').val(ui.values[0] + ' - ' + ui.values[1]);
+            $('#price input').val(ui.values[0] + ' - ' + ui.values[1]);
         }
     });
 
-    $('#amount').val(
-        $('#slider-range').slider('values', 0) + ' - ' + $('#slider-range').slider('values', 1)
+    $('#price input').val(
+        $('#price-slider-range').slider('values', 0) + ' - ' + $('#price-slider-range').slider('values', 1)
     );
 });
