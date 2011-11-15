7964ec90677e96f1a0de5fc5bcbc66f09e2bee09
Dennis Dietrich <d.dietrich84@googlemail.com>
2011-11-15 12:40:23 +0100

    updated js

--- js/form-fields.js
+++ js/form-fields.js
@@ -9,19 +9,39 @@
  */
 
 $(function () {
-    //$('#price').append('<div id="price-slider-range"></div>');
 
-    $('#price-slider-range').slider({
-        range: true,
-        min: 0,
-        max: 100,
-        values: [ 0, 20 ],
-        slide: function(event, ui) {
-            $('#price input').val(ui.values[0] + ' - ' + ui.values[1]);
-        }
+    /**
+     * The range slider.
+     *
+     * Replaces the original form types with a jquery-ui
+     * slider.
+     */
+    $('.form-range-row').each(function () {
+        var row         = $(this),
+            rangeSlider = $('<div class="slider-range"></div>').appendTo(row),
+            from        = row.find('div input').eq(0),
+            to          = row.find('div input').eq(1);
+
+        // Configure the jquery slider component
+        rangeSlider.slider({
+            range:  true,
+            min:    0,
+            max:    100,
+            values: [0, 20],
+            slide: function (event, ui) {
+                from.val(ui.values[0]);
+                to.val(ui.values[1]);
+            }
+        });
+
+        // Pre set the actual amount to the
+        // hidden form fields.
+        from.val(rangeSlider.slider('values', 0));
+        to.val(rangeSlider.slider('values', 1));
+
+        // Hides the original form fields.
+        // Ensure the posibility to fill the
+        // fields if javascript is disabled.
+        row.find('div').eq(0).css('opacity', 0);
     });
-
-    $('#price input').val(
-        $('#price-slider-range').slider('values', 0) + ' - ' + $('#price-slider-range').slider('values', 1)
-    );
 });
