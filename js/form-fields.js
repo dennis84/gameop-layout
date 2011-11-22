8c02d6ad144d002be526a5d6a2daad9c87030166
Dennis Dietrich <d.dietrich84@googlemail.com>
2011-11-22 18:31:05 +0100

    updated layout

--- js/form-fields.js
+++ js/form-fields.js
@@ -49,5 +49,22 @@ $(function () {
         //row.find('div').eq(0).css('opacity', 0);
     });
 
+    /**
+     * The rating row.
+     *
+     * Sets the background position of active
+     * radio button. The background positions have
+     * always 50px distance on the y axis to another.
+     */
+    $('.form-rating-row').each(function () {
+        var row    = $(this),
+            div    = $(this).find('div');
 
+        row.on('click', 'label', function () {
+            var index  = $(this).index('label'),
+                offset = index * 50;
+
+            div.css('backgroundPosition', '0 -' + offset + 'px');
+        });
+    });
 });
