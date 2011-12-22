37315c182b6397a05caa0e485f573f7468c301d6
dennis84 <d.dietrich84@googlemail.com>
2011-12-22 16:03:01 +0100

    updated layout

---
+++ js/labels.js
@@ -0,0 +1,14 @@
+/*
+ * This file is part of the gameop package.
+ *
+ * (c) Dennis Dietrich <d.dietrich84@googlemail.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$(function () {
+    $('.label-item').on('click', function () {
+        $(this).toggleClass('selected');
+    });
+});
