66fdb60be3a174e3b749100aca3ccdf80934f09f
dennis84 <d.dietrich84@googlemail.com>
2011-12-23 16:36:45 +0100

    updated layout

--- js/toggle-items.js
+++ js/toggle-items.js
@@ -12,10 +12,20 @@ $(function () {
     $('.label-item').on('click', function () {
         $(this).toggleClass('selected');
     });
-});
 
-$(function () {
-    $('.offer-item').on('click', function () {
-        $(this).toggleClass('selected');
+    $('.offer-item .icon').on('click', function () {
+        if ($(this).parent().hasClass('selected')) {
+            $(this).parent().removeClass('selected');
+        } else {
+            $(this).parent().addClass('selected');
+        }
+
+        $(this).next('input[type="checkbox"]')[0].click();
+    });
+
+    $('#offer-collection').submit(function () {
+        var data = $(this).serialize();
+        $(document).trigger('onOfferCollectionSubmit.gameop', [data]);
+        return false;
     });
 });
